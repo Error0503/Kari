0d1a7086135e4404316a8a5cf75644e6eff7ddc2
zsigmons <zsigmondsebi@gmail.com>
2020-12-12 12:24:05 +0100

    A cél az lenne. hogy ugyanabban a div-ben legyen az output, mint az input

--- kari.php
+++ kari.php
@@ -12,60 +12,57 @@
 				<br>
 				<input style="color: white; font-family: cursive; font-size: 20px; margin: 5px; padding: 6px 20px; border: 2px solid red; border-radius: 3px; background-color: red;" type="submit">
 			</form>
-		</div>
-		<?php
-			if ($_SERVER['REQUEST_METHOD'] == "POST") {
+			<?php
+				if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-				echo '<div style="text-align:center; font-family: cursive; margin:auto; width:55vw; border: 2px solid red; border-radius: 5px; padding: 10px; background-color: #004504; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">';
+					$conn = new mysqli("localhost", "debian-sys-maint", "30BO5y0nrmvdQDA3", "kari");
 
-				$conn = new mysqli("localhost", "debian-sys-maint", "30BO5y0nrmvdQDA3", "kari");
+					$name = $_POST["name"];													// Az illető saját neve
 
-				$name = $_POST["name"];													// Az illető saját neve
+					$tries = 0;
+					while ($tries < 35) {
 
-				$tries = 0;
-				while ($tries < 35) {
-
-					$sql = 'SELECT Pár FROM párok WHERE Név="' . $name . '"';							// Keresés párra
-					$result = $conn -> query($sql);
-					$row = $result -> fetch_assoc();
-
-					if ($row["Pár"] == "") {											// Van-e párod?
-						$sql = 'SELECT Név FROM párok WHERE Pár="' . $name . '"';						// Keresés hogy már pár vagy-e
-						$result = $conn ->query($sql);
+						$sql = 'SELECT Pár FROM párok WHERE Név="' . $name . '"';							// Keresés párra
+						$result = $conn -> query($sql);
 						$row = $result -> fetch_assoc();
 
-						if ($row["Név"] == "") {										// Párja vagy-e valakinek?
-							$sql = 'SELECT Név FROM párok WHERE Név!="' . $name . '" ORDER BY RAND() LIMIT 1';		// Véletlen név
-							$result = $conn -> query($sql);
+						if ($row["Pár"] == "") {											// Van-e párod?
+							$sql = 'SELECT Név FROM párok WHERE Pár="' . $name . '"';						// Keresés hogy már pár vagy-e
+							$result = $conn ->query($sql);
 							$row = $result -> fetch_assoc();
-							echo "A lehetséges pár " . $row["Név"] . "<br>";
-							$pair = $row["Név"];
 
-							$sql = 'SELECT Pár FROM párok WHERE Név="' . $pair . '"';					// Keresés a véletlen névre párként
-							$result = $conn -> query($sql);
-							$row = $result -> fetch_assoc();
-							echo "A lehetséges pár párja " . $row["Pár"] . "<br>";
+							if ($row["Név"] == "") {										// Párja vagy-e valakinek?
+								$sql = 'SELECT Név FROM párok WHERE Név!="' . $name . '" ORDER BY RAND() LIMIT 1';		// Véletlen név
+								$result = $conn -> query($sql);
+								$row = $result -> fetch_assoc();
+								echo "A lehetséges pár " . $row["Név"] . "<br>";
+								$pair = $row["Név"];
 
-							if ($row["Pár"] == "") {									// A véletlen név párja-e egy másik névnek
-								echo $pair . "<br>";
-								$sql = 'UPDATE párok SET Pár="' . $pair . '" WHERE Név="' . $name . '"';		// Pár elmentése
+								$sql = 'SELECT Pár FROM párok WHERE Név="' . $pair . '"';					// Keresés a véletlen névre párként
+								$result = $conn -> query($sql);
+								$row = $result -> fetch_assoc();
+								echo "A lehetséges pár párja " . $row["Pár"] . "<br>";
+
+								if ($row["Pár"] == "") {									// A véletlen név párja-e egy másik névnek
+									echo $pair . "<br>";
+									$sql = 'UPDATE párok SET Pár="' . $pair . '" WHERE Név="' . $name . '"';		// Pár elmentése
+									$result = $conn -> query($sql);
+								}
+							} else {
+								echo "Te már " . $row["Név"] . " párja vagy!";							// Valakinek már a párja vagy
+
+								$sql = 'UPDATE párok SET Pár="' . $row["Név"] . '" WHERE Név="' . $name . '"';			// Pár elmentése
 								$result = $conn -> query($sql);
 							}
+							$tries++;
+
 						} else {
-							echo "Te már " . $row["Név"] . " párja vagy!";							// Valakinek már a párja vagy
-
-							$sql = 'UPDATE párok SET Pár="' . $row["Név"] . '" WHERE Név="' . $name . '"';			// Pár elmentése
-							$result = $conn -> query($sql);
+							echo "A te párod " . $row["Pár"] . "<br>";								// Már van párod
+							$tries = 99;
+							break;
 						}
-						$tries++;
-
-					} else {
-						echo "A te párod " . $row["Pár"] . "<br>";								// Már van párod
-						$tries = 99;
-						break;
 					}
 				}
-			}
-			echo "</div>";
+		</div>
 	</body>
 </html>
